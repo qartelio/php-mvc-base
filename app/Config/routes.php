bccd96a44a2e76ab0c81b9fad109da1a2f4fb0a5
Eric <qartelio31@gmail.com>
2025-02-02 03:48:23 +0500

    ё

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -9,54 +9,6 @@ return [
         'controller' => 'home',
         'action' => 'index'
     ],
-    
-    // Маршруты для пользователей
-    'user/register' => [
-        'controller' => 'user',
-        'action' => 'register'
-    ],
-    'user/store' => [
-        'controller' => 'user',
-        'action' => 'store'
-    ],
-    'user/login' => [
-        'controller' => 'user',
-        'action' => 'login'
-    ],
-    'user/authenticate' => [
-        'controller' => 'user',
-        'action' => 'authenticate'
-    ],
-    'user/profile' => [
-        'controller' => 'user',
-        'action' => 'profile'
-    ],
-    'user/logout' => [
-        'controller' => 'user',
-        'action' => 'logout'
-    ],
-    
-    // Маршруты для учителей
-    'teacher/registration' => [
-        'controller' => 'teacher',
-        'action' => 'showRegistration'
-    ],
-    'teacher/register' => [
-        'controller' => 'teacher',
-        'action' => 'register'
-    ],
-    'teacher/login' => [
-        'controller' => 'teacher',
-        'action' => 'showLogin'
-    ],
-    'teacher/authenticate' => [
-        'controller' => 'teacher',
-        'action' => 'login'
-    ],
-    'teacher/dashboard' => [
-        'controller' => 'teacher',
-        'action' => 'dashboard'
-    ],
 
     // Маршруты для студентов
     'student/register' => [
